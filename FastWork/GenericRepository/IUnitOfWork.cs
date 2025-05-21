25926bcb72fc887bd45893f53e9df0cd778a1781
Elfledian <tohuuthinh1911@gmail.com>
2025-05-21 13:03:04 +0700

    add generic

---
+++ FastWork/GenericRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWork.GenericRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>()
+            where TEntity : class;
+
+        Task<int> Complete();
+        int CompleteV2();
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ FastWork/GenericRepository/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using FastWork.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWork.GenericRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TheShineDbContext _context;
+        private Hashtable? _repositories;
+
+        public UnitOfWork(TheShineDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public int CompleteV2()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>()
+            where TEntity : class
+        {
+            if (_repositories == null)
+            {
+                _repositories = new Hashtable();
+            }
+
+            var entityType = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(entityType))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+                var repositoryInstance = Activator.CreateInstance(
+                    repositoryType.MakeGenericType(typeof(TEntity)),
+                    _context
+                );
+                _repositories.Add(entityType, repositoryInstance);
+            }
+            return (IGenericRepository<TEntity>)_repositories[entityType]!;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
