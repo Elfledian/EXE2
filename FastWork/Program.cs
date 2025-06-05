ab5ffeba472a97191897576993089ccac2287956
Elfledian <tohuuthinh1911@gmail.com>
2025-06-05 17:28:17 +0700

    Update Program.cs

--- FastWork/Program.cs
+++ FastWork/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost5173",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("https://theshine.nhannguyen.site")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
