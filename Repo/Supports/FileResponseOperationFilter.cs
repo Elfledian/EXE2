c801c43dea6c829a78f717737e2eed7b153ca145
Elfledian <tohuuthinh1911@gmail.com>
2025-06-07 02:08:28 +0700

    Create FileResponseOperationFilter.cs

---
+++ Repo/Supports/FileResponseOperationFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Repo.Supports
+{
+    public class FileResponseOperationFilter : IOperationFilter
+    {   
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var isFileDownload = context.MethodInfo.Name.ToLower().Contains("download");
+
+            if (!isFileDownload)
+                return;
+
+            operation.Responses["200"] = new OpenApiResponse
+            {
+                Description = "File downloaded successfully",
+                Content = new Dictionary<string, OpenApiMediaType>
+                {
+                    ["application/octet-stream"] = new OpenApiMediaType
+                    {
+                        Schema = new OpenApiSchema
+                        {
+                            Type = "string",
+                            Format = "binary"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
