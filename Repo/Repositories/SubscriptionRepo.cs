87e6a039b59e6a54846d5159dcd91b1cc7c45180
Nguyen Ngo <nguyennhm.l.1922@gmail.com>
2025-06-19 17:13:56 +0700

    Modify Payment and add Subcription

---
+++ Repo/Repositories/SubscriptionRepo.cs
@@ -0,0 +1,19 @@
+﻿using Repo.Data;
+using Repo.Entities;
+using Repo.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repo.Repositories
+{
+    public class SubscriptionRepo : GenericRepository<Subscription>
+    {
+        public SubscriptionRepo(TheShineDbContext context) : base(context)
+        {
+        }
+        // Additional methods specific to Subscription can be added here
+    }
+}
